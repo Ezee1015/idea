4c766d5690432ba8bb7ae0a0d016f9326edc46db
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2025-07-25 19:08:13 -0300

    Ensure that only one instance of Idea is running

--- src/main.c
+++ src/main.c
@@ -1,41 +1,75 @@
+#include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "todo_list.h"
 #include "tui.h"
 #include "cli.h"
 
-int main(int argc, char *argv[]) {
-  load_file();
+const char *lock_path = "/tmp/lock.idea";
 
-  if (argc == 1) {
-    // TUI Version
-    window_app();
-  } else {
-    // CLI Version
-    for (int i=1; i<argc; i++) {
-      Action_return result = cli_parse_input(argv[i]);
-      switch (result.type) {
-        case RETURN_SUCCESS:
-        case RETURN_INFO:
-          if (result.message && strcmp(result.message, ""))
-            printf("Message from the %dº command (%s)\n\n", i, argv[i]);
-          break;
+bool lock_file() {
+  if (access(lock_path, F_OK) == 0) {
+    printf("Idea is already running...\n");
+    return false;
+  }
 
-        case RETURN_ERROR:
-          list_destroy(&todo_list, (void (*)(void *))free_todo);
-          printf("An ERROR occurred in the %dº command (%s)\n", i, argv[i]);
-          return 1;
+  FILE *lock_file = fopen(lock_path, "w");
+  if (!lock_file) {
+    printf("Error creating the lock...\n");
+    return false;
+  }
+
+  fclose(lock_file);
+  return true;
+}
+
+bool unlock_file() {
+  bool removed = (remove(lock_path) == 0);
+  if (!removed) printf("Unable to remove the lock file\n");
+  return removed;
+}
+
+bool parse_argv_cli(int argc, char *argv[]) {
+  for (int i=1; i<argc; i++) {
+    Action_return result = cli_parse_input(argv[i]);
+
+    switch (result.type) {
+      case RETURN_SUCCESS:
+      case RETURN_INFO:
+        if (result.message && strcmp(result.message, ""))
+          printf("Message from the %dº command (%s)\n\n", i, argv[i]);
+        break;
+
+      case RETURN_ERROR:
+        printf("An ERROR occurred in the %dº command (%s)\n", i, argv[i]);
+        return false;
 
       case RETURN_ERROR_AND_EXIT:
-          list_destroy(&todo_list, (void (*)(void *))free_todo);
-          printf("An ABORT occurred in the %dº command (%s)\n", i, argv[i]);
-          return 1;
-      }
+        printf("An ABORT occurred in the %dº command (%s)\n", i, argv[i]);
+        return false;
     }
-    if (todo_list_modified) print_todo(NULL);
+  }
+
+  if (todo_list_modified) print_todo(NULL);
+  return true;
+}
+
+int main(int argc, char *argv[]) {
+  if (!lock_file()) return 1;
+  load_file();
+
+  int ret;
+  if (argc == 1) {
+    // TUI Version
+    ret = window_app();
+  } else {
+    // CLI Version
+    ret = (parse_argv_cli(argc, argv)) ? 0 : 1;
   }
 
   if (todo_list_modified) save_file();
   list_destroy(&todo_list, (void (*)(void *))free_todo);
-  return 0;
+  if (!unlock_file()) return 1;
+  return ret;
 }
