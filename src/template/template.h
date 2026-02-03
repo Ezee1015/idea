0f063c2b185248e0b5400a3be24b0975b486d774
Ezee1015 <82286822+Ezee1015@users.noreply.github.com>
2026-02-03 11:08:07 -0300

    Add ToDo notes to the HTML template

--- src/template/template.h
+++ src/template/template.h
@@ -9,6 +9,8 @@
 
 // Macros to use in the HTML Template (to simplify the c code there)
 #define CSTR(cstr) if (fprintf(f, "%s", cstr) < 0) return FPRINTF_ERROR();
+#define CHAR(c) if (fputc(c, f) == EOF) return FPRINTF_ERROR();
+#define UINT(uint) if (fprintf(f, "%u", uint) < 0) return FPRINTF_ERROR();
 #define UNIX_TIME(time) if (fprintf(f, "%ld", *time) < 0) return FPRINTF_ERROR();
 #define ERROR(msg) ACTION_RETURN(RETURN_ERROR, msg)
 
